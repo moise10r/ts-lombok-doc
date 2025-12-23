c7150cb460be5e8db5ae73f8ca0ca63e75f90fdc
moise10r <moiserushanika2006@gmail.com>
2025-12-23 15:00:00 +0300

    components: add barrel export

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as CodeBlock } from './CodeBlock';
+export { default as FeatureCard } from './FeatureCard';
