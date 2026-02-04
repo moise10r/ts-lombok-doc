24b1f975c1325aa434103d8449222fd7afc5f1c6
moise10r <moiserushanika2006@gmail.com>
2026-02-04 15:00:00 +0300

    refactor: clean up exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as CodeBlock } from './CodeBlock';
 export { default as FeatureCard } from './FeatureCard';
+
